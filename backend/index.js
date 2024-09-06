872c26839cd059b50b69962e3be347f32fb0bcb1
Anant Shukla <anantshukla404@gmail.com>
2024-09-06 10:41:40 +0530

    single and array file storing in target directory using multer

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,17 @@ const multer = require('multer');
 const port = process.env.PORT || 3000;
 // const formData = new FormData();
 
-const upload = multer({dest: 'uploads/'});
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+    //   const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.originalname);
+    }
+  })
+  
+const upload = multer({ storage });
 
 app.get('/files', (req, res) => {
 
@@ -12,13 +22,17 @@ app.get('/files', (req, res) => {
     console.log("GET request successful !")
 });
 
-app.post('/files/upload', upload.single('file'),(req, res) => {
+app.post('/files/upload', upload.array('file'),(req, res) => {
 
-    // we can access file options using the request object
-
-    res.send(`Files uploaded successfully !`);
-    console.log("POST reqeust successful !")
-})
+    // // we can access file options using the request object
+    // console.log(`params`)
+    // console.log(req.params);
+    // console.log('body');
+    // console.log(req.body);
+    // res.send(`Files uploaded successfully !`);
+    res.json(req.file);
+    console.log("POST reqeust successful !");
+});
 
 app.listen(port, () => {
     console.log(`Server listening at port : ${port}`);
